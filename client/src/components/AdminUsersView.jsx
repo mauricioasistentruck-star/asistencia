ddaff879f57998c0e2602f6923e692411af0aa21
Mauricio Asistentruck <mauricio@asistentruck.cl>
2026-08-25 12:36:37 -0400

    Fix handleImportBackup undefined in AdminUsersView, update APK and bundle

--- client/src/components/AdminUsersView.jsx
+++ client/src/components/AdminUsersView.jsx
@@ -329,7 +329,7 @@ export default function AdminUsersView({ currentUser, theme }) {
                 ref={fileInputRef}
                 accept=".json"
                 className="hidden"
-                onChange={handleImportBackup}
+                onChange={handleImportFileChange}
                 disabled={backupLoading}
               />
             </div>
