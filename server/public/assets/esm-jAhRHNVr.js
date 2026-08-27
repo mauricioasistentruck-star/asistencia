1418f9894650b2e8d371f27cffa37cb473aecfd3
Mauricio Asistentruck <mauricio@asistentruck.cl>
2026-08-27 17:21:15 -0400

    Actualizacion de pagina de Render y aplicacion ControlAsistencia.apk

---
+++ server/public/assets/esm-jAhRHNVr.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DZGCInRZ.js","assets/index-DyMGNjQW.js","assets/index-CYCQ1tSF.css"])))=>i.map(i=>d[i]);
+import{r as e,t}from"./index-DyMGNjQW.js";var n=e(`App`,{web:()=>t(()=>import(`./web-DZGCInRZ.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
